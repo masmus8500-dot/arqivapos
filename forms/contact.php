93989052760abf45bface229ef8b74654cdb4165
masmus8500-dot <masmus8500@gmail.com>
2025-10-05 01:33:27 +0700

    pesan

--- forms/contact.php
+++ forms/contact.php
@@ -8,7 +8,7 @@
  */
 
 // Replace contact@example.com with your real receiving email address
-$receiving_email_address = 'contact@example.com';
+$receiving_email_address = 'arqivapos@gmail.com';
 
 if (file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php')) {
   include($php_email_form);
@@ -16,7 +16,6 @@ if (file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-for
   die('Unable to load the "PHP Email Form" Library!');
 }
 
-$contact = new PHP_Email_Form;
 $contact->ajax = true;
 
 $contact->to = $receiving_email_address;
